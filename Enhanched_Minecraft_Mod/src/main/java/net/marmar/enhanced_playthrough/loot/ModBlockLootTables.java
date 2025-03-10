298b298e4b0d22f4e43b9a817e11c8afbb67f0e6
Mar Mar <trespoderososminecraft@gmail.com>
2025-03-10 17:10:24 -0300

    Corrected a bug where ores doesn´t gave diamond and emerald
    Changelog:
    - Corrected a bug where ores doesn´t gave diamond and emerald

--- Enhanched_Minecraft_Mod/src/main/java/net/marmar/enhanced_playthrough/loot/ModBlockLootTables.java
+++ Enhanched_Minecraft_Mod/src/main/java/net/marmar/enhanced_playthrough/loot/ModBlockLootTables.java
@@ -16,6 +16,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
